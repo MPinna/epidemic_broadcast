86f9f0e8d5ac9728e9c33f49c68e8144d4bc4d8b
MPinna <marco.pinn95@gmail.com>
2021-03-12 02:10:58 +0100

    Add sleep coverage fetcher

--- python/fetcher.py
+++ python/fetcher.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize.zeros import results_c
 def read_collisions(file):
     df = pd.read_csv(file, dtype={"name":"string", "count":int})
     collisionsDF = df[df['name'] == 'packetDropIncorrectlyReceived:count']
@@ -39,3 +40,33 @@ def read_duration(file):
         coverageList = list(map(int, vecvalueDF["vecvalue"][i].split()))
         totalCoverageSlot.append(coverageList[len(coverageList)-1])
     return totalCoverageSlot
+
+# return a list of couples containing the sleep coverage (vector) and the
+# current slot number when the target host received the message (namely
+# the slot where the system transitioned to state S)
+def read_sleeping_coverage(file):  
+    df = pd.read_csv(file, dtype={"name":"string", "count":int})
+    stateS_DF = df[df['name'] == 'stateSTransitionStat:last']
+    stateS_DF = stateS_DF[['run', 'name', 'value']]
+    stateS_DF = stateS_DF.groupby(["run"], as_index = False)["value"].first()
+    stateS_DF = list(stateS_DF['value'])
+    # usersDF = stateSDF.groupby(["module"], as_index = False)["value"].first()
+    # stateSDF = stateSDF.groupby(["run"], as_index = False)["value"].sum()
+    sleepDF = df[df['name'] == 'sleepCoverageStat:vector']
+    sleepDF = sleepDF[['run', 'name', 'value', 'vecvalue']]
+    vecvalueDF = sleepDF.groupby(["run"], as_index = False)["vecvalue"].first()
+    vecvalueDF = list(vecvalueDF['vecvalue'])
+
+    iterations = len(vecvalueDF)
+    result = []
+    for i in range(iterations):
+        couple = {}
+        
+        # all the slots where hosts went to sleep, similar to timeCoverageStat
+        couple["vector"] = list(map(int, vecvalueDF[i].split()))
+
+        # slot number where the system transitioned to state S
+        # (NaN if the target was never reached by the broadcast)
+        couple["stateSSlot"] = stateS_DF[i]
+        result.append(couple)
+    return result
